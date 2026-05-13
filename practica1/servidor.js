346c49127d4e8df53aac1ae22fb9269a38182423
ramdom123046311 <123046311@upq.edu.mx>
2026-05-12 19:11:09 -0600

    Js

---
+++ practica1/servidor.js
@@ -0,0 +1,18 @@
+console.log("hola mundo js desde el servidor")
+
+let edad1=11
+const edad2=42
+
+console.log("edad promedio")
+console.log((edad1+edad2)/2)
+ 
+/*Medir tiempo de un proceso */
+console.time('miproceso')
+   for(let i=0; i < 10000000000; i++){}
+console.timeEnd('miproceso')
+
+let usuarios= [
+    {nombre:"Manuel", Edad:21},
+    {nombre:"Martin", Edad:22}
+]
+console.table(usuarios)
