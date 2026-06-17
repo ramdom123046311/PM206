2c0202c0dc02cf4361dd2d746919b4abd66c1941
ramdom123046311 <123046311@upq.edu.mx>
2026-06-16 18:45:33 -0600

    Stylesheets

--- practica5/miapp2/App.js
+++ practica5/miapp2/App.js
@@ -9,11 +9,12 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <Perfil nombre= "Manuel"carrera = "sistemas" materia= "P movil" cuatrimestre ="9no"></Perfil>
-      <Text>---------------------------------------------------------------------------------------</Text>
-      <Perfil nombre= "Daniel"carrera = "mecatronica" materia= "embebidos" cuatrimestre ="8vo"></Perfil>
+      <Perfil style={styles.tarjetaRoja} nombre= "Manuel"carrera = "sistemas" materia= "P movil" cuatrimestre ="9no"></Perfil>
+      <Text>-------------------------------------------------------------------------------------------------------------</Text>
+      <Perfil style={styles.TarjetaVerde} nombre= "Daniel"carrera = "mecatronica" materia= "embebidos" cuatrimestre ="8vo"></Perfil>
       <StatusBar style="auto" />
 
+<Perfil style={styles.tarjetaRoja} nombre= "Jack"carrera = "sistemas" materia= "P movil" cuatrimestre ="6to"></Perfil>
     </View>
   );
 }
@@ -23,6 +24,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'Center',
+    flexDirection:'center'
   },
+
+  tarjetaRoja:{backgroundColor:'#FF6B6B'},
+  TarjetaVerde:{backgroundColor:'#1fc427'},
+  
 });
--- practica5/miapp2/components/Perfil.js
+++ practica5/miapp2/components/Perfil.js
@@ -1,18 +1,18 @@
-import { Button,Text,Image,View } from "react-native";
+import { Button,Text,Image,View,StyleSheet } from "react-native";
 import React,{useState} from "react";
 //Perfil desustructuracion
-export const Perfil= ({nombre,carrera,materia,cuatrimestre}) => {
+export const Perfil= ({nombre,carrera,materia,cuatrimestre,style}) => {
    const [mostrar,setMostrar] = useState(false);
     //Renderizado condicional
     return (
-        <View>
-            <Text>{nombre}</Text>
+        <View style={[estilos.tarjeta,style]}>
+            <Text style= {estilos.nombre}>{nombre}</Text>
 
             { mostrar && 
             <>
-            <Text>{carrera}</Text>
-            <Text>{materia}</Text>
-            <Text>{cuatrimestre}</Text>
+            <Text style={estilos.carrea}>{carrera}</Text>
+            <Text style={estilos.otroTexto}>{materia}</Text>
+            <Text style={estilos.otroTexto}>{cuatrimestre}</Text>
             </>
             }
             <Button
@@ -22,6 +22,33 @@ export const Perfil= ({nombre,carrera,materia,cuatrimestre}) => {
         </View>
     );
 }
+
+const estilos = StyleSheet.create({
+nombre:{
+        fontSize:24,
+        fontWeight: 700,
+        textTransform:"uppercase"
+   },
+carrea:{
+    fontSize: 18,
+    color: 'blue',
+    fontFamily:'roboto'
+},
+
+otroTexto:{
+
+    fontSize: 12,
+    fontFamily: 'Courier',
+    fontStyle: 'italic'
+},
+
+tarjeta:{
+   borderWidth: 3,
+   margin: 20,
+   padding:25,
+},
+
+});
 
 /* export const Perfil= (props) =>{
     return(
